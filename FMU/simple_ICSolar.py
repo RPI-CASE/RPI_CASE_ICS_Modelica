edcc31633a1ccd887558e3e3c7827125e50811bb
JShultz89 <jss5237@gmail.com>
2015-04-17 13:14:39 -0400

    changing model for FMU setup

--- FMU/simple_ICSolar.py
+++ FMU/simple_ICSolar.py
@@ -15,20 +15,20 @@ opts = myModel.simulate_options()
 # opts["tfinal"] = 86400.0 #End time of simulation in seconds
 opts['solver'] = 'CVode' # Solver Description: https://computation.llnl.gov/casc/sundials/description/description.html
 opts["ncp"] = steps #steps # Change the number of communication points
-opts['initialize'] = True
-opts['CVode_options']["maxh"] = 10
-opts['CVode_options']["report_continuously"] = True
-opts['CVode_options']["clock_step"] = True
+opts['initialize'] = False
+# opts['CVode_options']["maxh"] = 10
+# opts['CVode_options']["report_continuously"] = True
+# opts['CVode_options']["clock_step"] = True
 
-opts_b = myModel.simulate_options()
-# opts["tfinal"] = 86400.0 #End time of simulation in seconds
-opts_b['solver'] = 'CVode' # Solver Description: https://computation.llnl.gov/casc/sundials/description/description.html
-opts_b["ncp"] = (24*5) # Change the number of communication points
+# opts_b = myModel.simulate_options()
+# # opts["tfinal"] = 86400.0 #End time of simulation in seconds
+# opts_b['solver'] = 'CVode' # Solver Description: https://computation.llnl.gov/casc/sundials/description/description.html
+# opts_b["ncp"] = (24*5) # Change the number of communication points
 
-opts_c = myModel.simulate_options()
-# opts["tfinal"] = 86400.0 #End time of simulation in seconds
-opts_c['solver'] = 'CVode' # Solver Description: https://computation.llnl.gov/casc/sundials/description/description.html
-opts_c["ncp"] = (24*100) # Change the number of communication points
+# opts_c = myModel.simulate_options()
+# # opts["tfinal"] = 86400.0 #End time of simulation in seconds
+# opts_c['solver'] = 'CVode' # Solver Description: https://computation.llnl.gov/casc/sundials/description/description.html
+# opts_c["ncp"] = (24*100) # Change the number of communication points
 
 # rtol = 1.0e-4
 # atol = rtol*0.01
@@ -50,12 +50,34 @@ opts_c["ncp"] = (24*100) # Change the number of communication points
 
 # myModel.set(vars, values)
 
-NumOfModules = myModel.get('NumOfModules')
-# time = time_current+3600*14
-# myModel.set('time',time)
-print NumOfModules
+myModel.initialize()
 
-res = myModel.simulate(start_time=0,final_time=time_plus, options=opts)
+NumOfModules = myModel.get('NumOfModules')
+print "Number of Modules = " + str(NumOfModules)
+
+heatGen = [None]*max(range(0,50+1))
+DNI = [None]*max(range(0,50+1))
+timestamp = [None]*max(range(0,50+1))
+Q_module1 = [None]*max(range(0,50+1))
+Q_module2 = [None]*max(range(0,50+1))
+Q_module3 = [None]*max(range(0,50+1))
+Q_module4 = [None]*max(range(0,50+1))
+Q_module5 = [None]*max(range(0,50+1))
+Q_module6 = [None]*max(range(0,50+1))
+
+for i in range(0,50):
+	# DNI[i] = myModel.get("ics_envelopecassette1.DNI")
+	DNI[i] = 10*i
+	# myModel.set("ics_envelopecassette1.DNI", DNI[i]) 
+	res = myModel.simulate(start_time=86400/2.0+10*i,final_time=86400/2.0+10*(i+1), input=("ics_envelopecassette1.ics_selfshading1.DNI_in", DNI[i]), options=opts)
+	timestamp[i] = res['time'][0]
+	Q_module1[i] = res['ics_envelopecassette1.ics_stack1.iCS_Module[1].modulereceiver1.water_Block_HX1.Q_module'][-1]
+	Q_module2[i] = res['ics_envelopecassette1.ics_stack1.iCS_Module[2].modulereceiver1.water_Block_HX1.Q_module'][-1]
+	Q_module3[i] = res['ics_envelopecassette1.ics_stack1.iCS_Module[3].modulereceiver1.water_Block_HX1.Q_module'][-1]
+	Q_module4[i] = res['ics_envelopecassette1.ics_stack1.iCS_Module[4].modulereceiver1.water_Block_HX1.Q_module'][-1]
+	Q_module5[i] = res['ics_envelopecassette1.ics_stack1.iCS_Module[5].modulereceiver1.water_Block_HX1.Q_module'][-1]
+	Q_module6[i] = res['ics_envelopecassette1.ics_stack1.iCS_Module[6].modulereceiver1.water_Block_HX1.Q_module'][-1]
+	heatGen[i] = [Q_module1[i],Q_module2[i],Q_module3[i],Q_module4[i],Q_module5[i],Q_module6[i]]
 
 # Tstart = time_current
 # myModel.time = Tstart
@@ -69,26 +91,21 @@ res = myModel.simulate(start_time=0,final_time=time_plus, options=opts)
 
 # res_c = myModel.simulate(start_time=time_current+3600*14, final_time=time_current+3600*15, options=opts_c)
 
-timestamp = res['time']
+
 # timestamp_b = res_b['time']
 # timestamp_c = res_c['time']
 
-for i in range(1,NumOfModules+1):
-	Module_string = 'ics_envelopecassette1.ics_stack1.iCS_Module['+str(i)+'].modulereceiver1.water_Block_HX1.Q_module'
-	print Module_string
-	Q_module[i] = res[Module_string]
-	print Q_module[i]
+# for i in range(1,NumOfModules+1):
+# 	Module_string = 'ics_envelopecassette1.ics_stack1.iCS_Module['+str(i)+'].modulereceiver1.water_Block_HX1.Q_module'
+# 	print Module_string
+# 	Q_module[i] = res[Module_string]
+# 	print Q_module[i]
 
-Q_module1 = res['ics_envelopecassette1.ics_stack1.iCS_Module[1].modulereceiver1.water_Block_HX1.Q_module']
-Q_module2 = res['ics_envelopecassette1.ics_stack1.iCS_Module[2].modulereceiver1.water_Block_HX1.Q_module']
-Q_module3 = res['ics_envelopecassette1.ics_stack1.iCS_Module[3].modulereceiver1.water_Block_HX1.Q_module']
-Q_module4 = res['ics_envelopecassette1.ics_stack1.iCS_Module[4].modulereceiver1.water_Block_HX1.Q_module']
-Q_module5 = res['ics_envelopecassette1.ics_stack1.iCS_Module[5].modulereceiver1.water_Block_HX1.Q_module']
-Q_module6 = res['ics_envelopecassette1.ics_stack1.iCS_Module[6].modulereceiver1.water_Block_HX1.Q_module']
-cellTemp1 = res['ics_envelopecassette1.ics_stack1.iCS_Module[1].modulereceiver1.water_Block_HX1.thermalresistor_celltoreceiver.port_b.T']
-cellTemp2 = res['ics_envelopecassette1.ics_stack1.iCS_Module[2].modulereceiver1.water_Block_HX1.thermalresistor_celltoreceiver.port_b.T']
 
-heatGen = [Q_module1,Q_module2,Q_module3,Q_module4,Q_module5,Q_module6]
+# cellTemp1 = res['ics_envelopecassette1.ics_stack1.iCS_Module[1].modulereceiver1.water_Block_HX1.thermalresistor_celltoreceiver.port_b.T']
+# cellTemp2 = res['ics_envelopecassette1.ics_stack1.iCS_Module[2].modulereceiver1.water_Block_HX1.thermalresistor_celltoreceiver.port_b.T']
+
+
 
 # cellTemp1_b = res_b['ics_envelopecassette1.ics_stack1.iCS_Module[1].modulereceiver1.water_Block_HX1.thermalresistor_celltoreceiver.port_b.T']
 
@@ -96,7 +113,8 @@ heatGen = [Q_module1,Q_module2,Q_module3,Q_module4,Q_module5,Q_module6]
 
 
 plt.figure(1)
-plt.plot(timestamp, heatGen[0], 'r--')
+plt.plot(timestamp, Q_module1, 'r--')
+plt.plot(timestamp, DNI, 'b')
 plt.show()
 
 # plt.plot(timestamp, cellTemp1, 'r--')
@@ -104,7 +122,5 @@ plt.show()
 # plt.plot(timestamp_c, cellTemp1_c, 'g')
 # plt.show()
 
-print timestamp
-print heatGen
 # print cellTemp1
 # print cellTemp2
