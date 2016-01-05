4feb0ba9d9e923aa8ae9efd04d2436afe2365fd7
Justin Shultz <jss5237@gmail.com>
2016-01-05 13:17:46 -0500

    deleted some files

--- Archive/Modelica Scirpting/ICSolar.ICS_Skeleton_13opt.h
+++
@@ -1,9 +0,0 @@
-#if defined(_MSC_VER)
-extern "C" {
-#endif
-  int ICSolar_ICS_Skeleton_mayer(DATA* data, modelica_real* res);
-  int ICSolar_ICS_Skeleton_lagrange(DATA* data, modelica_real* res);
-  int ICSolar_ICS_Skeleton_pickUpBoundsForInputsInOptimization(DATA* data, modelica_real* min, modelica_real* max, modelica_real*nominal, modelica_boolean *useNominal, char ** name, modelica_real * start, modelica_real * startTimeOpt);
-#if defined(_MSC_VER)
-}
-#endif
